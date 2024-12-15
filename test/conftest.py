5c78213ea1b99a0b7c7bf8103d85f1631132c418
pkrecz <pkrecz@poczta.onet.pl>
2024-12-15 16:53:33 +0100

    Test module added.

---
+++ test/conftest.py
@@ -0,0 +1,102 @@
+import os
+import asyncio
+import pytest
+import pytest_asyncio
+import logging
+from asgi_lifespan import LifespanManager
+from typing import AsyncGenerator
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from config.database import get_db
+from app_example.models import Base
+from main import app
+from sqlalchemy.sql import text
+
+
+@pytest.fixture(scope="session")
+async def async_engine():
+
+    _engine = create_async_engine(os.getenv("DATABASE_URL_TEST"))
+
+    async with _engine.begin() as engine:
+        await engine.run_sync(Base.metadata.drop_all)
+        await engine.run_sync(Base.metadata.create_all)
+    logging.info("Configuration -----> Tables for testing has been created.")
+
+    yield _engine
+
+    async with _engine.begin() as engine:
+        await engine.run_sync(Base.metadata.drop_all)
+    logging.info("Configuration -----> Tables for testing has been removed.")
+
+
+
+@pytest.fixture(scope="session")
+async def async_session(async_engine) -> AsyncGenerator[AsyncSession, None]:
+
+    _session = async_sessionmaker(
+                                    autocommit=False,
+                                    autoflush=False,
+                                    expire_on_commit=False,
+                                    bind=async_engine)   
+    logging.info("Configuration -----> Session created.")
+    async with _session() as session:
+
+        await session.begin()
+        logging.info("Configuration -----> Session started.")
+        yield session
+        await session.rollback()
+        logging.info("Configuration -----> Rollback executed.")
+        await session.close()
+        logging.info("Configuration -----> Session closed.")
+
+        for table in reversed(Base.metadata.sorted_tables):
+            await session.execute(text(f"TRUNCATE {table.name} CASCADE;"))
+            await session.commit()
+        logging.info("Configuration -----> Truncate has been done.")
+
+
+@pytest_asyncio.fixture(scope="session")
+async def async_client(event_loop, async_session: AsyncSession) -> AsyncGenerator[AsyncClient, None]:
+
+    def override_get_db():
+        try:
+            yield async_session
+        finally:
+            async_session.close()
+    
+    app.dependency_overrides[get_db] = override_get_db
+    logging.info("Configuration -----> Dependency overrided.")
+
+    async with LifespanManager(app):
+       
+        async with AsyncClient(
+                        transport=ASGITransport(app=app),
+                        base_url="http://testserver") as client:
+            logging.info("Configuration -----> Client ready for running.")
+            yield client
+            logging.info("Configuration -----> Client finished job.")
+
+
+@pytest_asyncio.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture()
+def data_test_create_author():
+    return {
+            "name": "JohnyG",
+            "pseudo": "Walker",
+            "city": "LA"}
+
+
+@pytest.fixture()
+def data_test_update_author():
+    return {
+            "pseudo": "Ranger"}
---
+++ test/test.py
@@ -0,0 +1,55 @@
+import os
+
+
+async def test_create_author(
+                                async_client,
+                                data_test_create_author):
+    response = await async_client.post(
+                                        url=f"/author/",
+                                        json=data_test_create_author)
+    response_json = response.json()
+    assert response_json["name"] == data_test_create_author["name"]
+    assert response_json["pseudo"] == data_test_create_author["pseudo"]
+    assert response_json["city"] == data_test_create_author["city"]
+    assert response.status_code == 201
+    os.environ["ID"] = str(response_json["id"])
+
+
+async def test_update_author(
+                                async_client,
+                                data_test_create_author,
+                                data_test_update_author):
+    id = os.environ["ID"]
+    response = await async_client.put(
+                                        url=f"/author/{id}/",
+                                        json=data_test_update_author)
+    response_json = response.json()
+    assert response_json["name"] == data_test_create_author["name"]
+    assert response_json["pseudo"] == data_test_update_author["pseudo"]
+    assert response_json["city"] == data_test_create_author["city"]
+    assert response.status_code == 200
+
+
+async def test_retrive_author(
+                                async_client):
+    id = os.environ["ID"]
+    response = await async_client.get(
+                                        url=f"/author/{id}/")
+    response_json = response.json()
+    assert response_json["id"] == int(id) 
+    assert response.status_code == 200
+
+
+async def test_list_author(
+                                async_client):
+    response = await async_client.get(
+                                        url=f"/author/")
+    assert response.status_code == 200
+
+
+async def test_delete_author(
+                                async_client):
+    id = os.environ["ID"]
+    response = await async_client.delete(
+                                        url=f"/author/{id}/")
+    assert response.status_code == 204
